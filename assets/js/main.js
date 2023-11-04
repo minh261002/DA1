d1bdd531c5b22c9b3b258655e96e5283987305bd
Loc <locvh12345@gmail.com>
2023-11-04 19:12:38 +0700

    giao dien trang chu

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+const btnOpenSideBar = document.querySelector('.icon-menu');
+const sideBar = document.querySelector('.side__bar');
+const overlay = document.querySelector('#overlay');
+const btnCloseSideBar = document.querySelector('.close-btn');
+
+
+btnOpenSideBar.addEventListener('click', function (e) {
+    sideBar.classList.toggle("active");
+    overlay.style.display = 'block';
+    sideBar.style.animationName = "openNav";
+})
+
+btnCloseSideBar.addEventListener('click', function (e) {
+    sideBar.style.animationName = "closeNav";
+
+    setTimeout(function () {
+        sideBar.classList.remove("active");
+        overlay.style.display = 'none';
+    }, 350)
+})
+
+overlay.addEventListener('click', function (e) {
+
+    sideBar.style.animationName = "closeNav";
+
+    setTimeout(function () {
+        sideBar.classList.remove("active");
+        overlay.style.display = 'none';
+    }, 350)
+})
